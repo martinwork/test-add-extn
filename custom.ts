b7e15659e1aed7b2d8233cbcf91dc55de41b85a5
Martin Williams <martinwork@users.noreply.github.com>
2024-04-22 19:30:21 +0100

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,15 @@
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace custom {
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% block
+    export function add(value: number): number {
+        return value + 1;
+    }
+}
